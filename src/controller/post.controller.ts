f33435a03f0502a8716d7c5d34cd62e278f68b8b
martins <chizobaebuka1@gmail.com>
2024-10-10 13:50:07 +0100

    Add post controller and routes, update user controller and auth middleware

---
+++ src/controller/post.controller.ts
@@ -0,0 +1,75 @@
+import { Request, Response } from 'express';
+import PostModel from '../db/models/postmodel';
+import { AuthRequest } from '../middleware/authMiddleware';
+
+export const createPost = async (req: AuthRequest, res: Response): Promise<void> => {
+    try {
+      const { title, content } = req.body;
+  
+      // Ensure that the user is authenticated and user object exists in the request
+      if (!req.user || !req.user.id) {
+        res.status(401).json({ message: "User not authenticated" });
+      }
+  
+      const userId = req.user.id;
+  
+      // Create the post in the database
+      const post = await PostModel.create({
+        title,
+        content,
+        userId,  // Associate the post with the authenticated user
+      });
+  
+      // Respond with the newly created post
+      res.status(201).json({
+        status: 'success',
+        message: 'Post created successfully',
+        data: post,
+      });
+    } catch (error: any) {
+      res.status(500).json({
+        message: 'Error creating post',
+        error: error.message || error.errors,
+      });
+    }
+  };
+
+export const getPostById = async(req: Request, res: Response) => {
+    try {
+        const postId = req.params.id
+        const post = await PostModel.findOne({ where: { id: postId }})
+
+        if (!post) {
+            return res.status(404).json({ message: 'Post not found' });
+        }
+
+        return res.status(200).json({
+            status:'success',
+            message: 'Post retrieved successfully',
+            data: post,
+        });
+    } catch (error: any) {
+        res.status(500).json({ message: 'Error retrieving post', error: error.errors });
+        return;
+    }
+}
+
+export const getUserPosts = async (req: Request, res: Response) => {
+    try {
+        const userId = req.params.id
+        const posts = await PostModel.findAll({ where: { userId } })
+
+        if (!posts) {
+            return res.status(404).json({ message: 'No posts found for this user' });
+        }
+
+        return res.status(200).json({
+            status:'success',
+            message: 'Posts retrieved successfully',
+            data: posts,
+        });
+    } catch (error: any) {
+        res.status(500).json({ message: 'Error retrieving posts', error: error.errors });
+        return;
+    }
+}
